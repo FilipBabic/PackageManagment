5feb6b9b3a1a77f279633d9aa48186a10ec29bc7
FilipBabic <41800756+FilipBabic@users.noreply.github.com>
2023-09-28 02:19:04 +0200

    Update db-config.php

--- db-config.php
+++ db-config.php
@@ -1,13 +1,1 @@
-<?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'rootnew');
-define('DB_PASSWORD', 'Oldschool!2022');
-define('DB_NAME', 'johnuser_upravljanje_paketima');
-
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if($conn === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
