66a46e8153e2206225a77103d8fdacee4985bd23
FilipBabic <41800756+FilipBabic@users.noreply.github.com>
2023-09-28 02:30:23 +0200

    Update calculate-kurir.php

--- calculate-kurir.php
+++ calculate-kurir.php
@@ -1,6 +1,6 @@
 <?php
 
-    $connect = new PDO("mysql:host=localhost;dbname=*********","rootnew","************");
+    $connect = new PDO("mysql:host=localhost;dbname=**********","rootnew","**********");
 
     if($_POST != '') 
     {                       
